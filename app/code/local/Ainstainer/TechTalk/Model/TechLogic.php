a75b60e5b7408aedb611350778904e710e516db9
Lyudmila Antipova <melina.antipova@gmail.com>
2016-10-28 13:45:36 +0300

    first

---
+++ app/code/local/Ainstainer/TechTalk/Model/TechLogic.php
@@ -0,0 +1,14 @@
+<?php
+class Ainstainer_TechTalk_Model_TechLogic extends Mage_Core_Model_Abstract
+{
+
+    public function _construct()
+    {
+        parent::_construct();
+        $this->_init('techtalk/techLogic');
+    }
+
+    public function sayHello(){
+        echo 'Hello';
+    }
+}
